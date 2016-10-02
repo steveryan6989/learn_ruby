7220a06afcbd934c07bcd236a3653a8fae987a08
Steve Ryan <steveryan6989@gmail.com>
2016-10-02 17:28:43 -0400

    finished the book titles example

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,13 @@
 #write your code here
+def translate(input)
+		
+		if input[0]=="e"||"a"||"i"||"o"||"u"
+			input=input+"ay"
+		else
+			input=input[1..-1]+input[0]
+			input=input+"ay"
+		end
+		
+
+	
+end
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,12 @@
 class Book
-# write your code here
+	def title=(word)
+		bad_words=["a","an","and","or","the","of","in","on","over","above"]
+		@title=word.split.map{|x| bad_words.include?(x) ? x : x.capitalize}
+		@title[0]=@title[0].capitalize
+		@title=@title.join(" ")
+	end
+	def title
+		@title
+	end
+
 end
