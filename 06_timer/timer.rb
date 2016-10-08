1a122b0eb6ee6047554fca6fd121be958568c1d5
Steve Ryan <steveryan6989@gmail.com>
2016-10-07 21:22:50 -0400

    finished the timer problem

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,14 @@
 class Timer
-  #write your code here
+	def initialize
+		@seconds=0
+	end
+  	def seconds=(seconds)
+		@seconds=seconds
+	end
+	def seconds
+		@seconds
+	end
+	def time_string
+		timestring=Time.at(@seconds).utc.strftime("%H:%M:%S")
+	end
 end
